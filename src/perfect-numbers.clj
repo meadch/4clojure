d983adba818457583e17da7f5b946db4f98a873f
meadch <meadch@gmail.com>
2020-10-07 08:14:23 -0700

    perfect-numbers

---
+++ src/perfect-numbers.clj
@@ -0,0 +1,20 @@
+
+;; # 80 Perfect Numbers
+;; http://www.4clojure.com/problem/80
+;; A number is "perfect" if the sum of its divisors equal the number itself. 6 is a perfect number because 1+2+3=6. Write a function which returns true for perfect numbers and false otherwise.
+
+(defn perfect-number? [n] true)
+
+(= (perfect-number? 6) true)
+
+
+(= (perfect-number? 7) false)
+
+
+(= (perfect-number? 496) true)
+
+
+(= (perfect-number? 500) false)
+
+
+(= (perfect-number? 8128) true)
