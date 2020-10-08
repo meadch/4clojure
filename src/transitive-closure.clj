325db102ed726d69fc8e8be5ed3dc665d84dc9e3
meadch <meadch@gmail.com>
2020-10-07 18:21:20 -0700

    transitive-closure solution

--- src/transitive-closure.clj
+++ src/transitive-closure.clj
@@ -4,19 +4,25 @@
 
 ; Write a function which generates the transitive closure of a binary relation. The relation will be represented as a set of 2 item vectors.
 
+(defn transitive-closure [s]
+  (let [mapping (zipmap (map first s) (map last s))
+        make-relations (fn [k] (loop [key k val (get mapping key) relations []]
+                                 (if (contains? mapping key)
+                                   (recur val (get mapping val) (conj relations [k val]))
+                                   relations)))]
+    (set (mapcat make-relations (keys mapping)))))
 
 (let [divides #{[8 4] [9 3] [4 2] [27 9]}]
-  (= (__ divides) #{[4 2] [8 4] [8 2] [9 3] [27 9] [27 3]}))
-
+  (= (transitive-closure divides) #{[4 2] [8 4] [8 2] [9 3] [27 9] [27 3]}))
 
 (let [more-legs
       #{["cat" "man"] ["man" "snake"] ["spider" "cat"]}]
-  (= (__ more-legs)
+  (= (transitive-closure more-legs)
      #{["cat" "man"] ["cat" "snake"] ["man" "snake"]
        ["spider" "cat"] ["spider" "man"] ["spider" "snake"]}))
 
 (let [progeny
       #{["father" "son"] ["uncle" "cousin"] ["son" "grandson"]}]
-  (= (__ progeny)
+  (= (transitive-closure progeny)
      #{["father" "son"] ["father" "grandson"]
        ["uncle" "cousin"] ["son" "grandson"]}))
