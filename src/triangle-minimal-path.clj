e39729ba3d71ee23e5426f3174cd0e778931b891
meadch <meadch@gmail.com>
2020-10-11 09:21:28 -0700

    triangle-minimal-path solution

--- src/triangle-minimal-path.clj
+++ src/triangle-minimal-path.clj
@@ -3,14 +3,24 @@
 
 ; Write a function which calculates the sum of the minimal path through a triangle. The triangle is represented as a collection of vectors. The path should start at the top of the triangle and move to an adjacent number on the next row until the bottom of the triangle is reached.
 
-(defn f [t] t)
+(defn sum-min-path
+  ([t] (sum-min-path (vec t) 0 0))
+  ([t row cell]
+   (let [val (get-in t (list row cell))]
+     (+ val (if (nil? (get t (inc row)))
+              0
+              (min
+               (sum-min-path t (inc row) cell)
+               (sum-min-path t (inc row) (inc cell))))))))
 
-(= 7 (f '([1]
+(= 7 (sum-min-path 
+        '([1]
           [2 4]
           [5 1 4]
           [2 3 4 5]))) ; 1->2->1->3
 
-(= 20 (f '([3]
+(= 20 (sum-min-path 
+         '([3]
            [2 4]
            [1 9 3]
            [9 9 2 4]
