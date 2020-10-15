6341f87a56fa1a7fa757fe69247c2e355d55f183
meadch <meadch@gmail.com>
2020-10-15 12:06:59 -0700

    to-tree-or-not-to-tree solution

--- src/to-tree-or-not-to-tree.clj
+++ src/to-tree-or-not-to-tree.clj
@@ -3,30 +3,28 @@
 
 ;Write a predicate which checks whether or not a given sequence represents a binary tree. Each node in the tree must have a value, a left child, and a right child.
 
+(defn bin-tree? [node]
+  (and (coll? node)
+       (= 3 (count node))
+       (every? #(or (nil? %) (bin-tree? %)) (rest node))))
 
-(= (__ '(:a (:b nil nil) nil))
+(= (bin-tree? '(:a (:b nil nil) nil))
    true)
 
-
-(= (__ '(:a (:b nil nil)))
+(= (bin-tree? '(:a (:b nil nil)))
    false)
 
-
-(= (__ [1 nil [2 [3 nil nil] [4 nil nil]]])
+(= (bin-tree? [1 nil [2 [3 nil nil] [4 nil nil]]])
    true)
 
-
-(= (__ [1 [2 nil nil] [3 nil nil] [4 nil nil]])
+(= (bin-tree? [1 [2 nil nil] [3 nil nil] [4 nil nil]])
    false)
 
-
-(= (__ [1 [2 [3 [4 nil nil] nil] nil] nil])
+(= (bin-tree? [1 [2 [3 [4 nil nil] nil] nil] nil])
    true)
 
-
-(= (__ [1 [2 [3 [4 false nil] nil] nil] nil])
+(= (bin-tree? [1 [2 [3 [4 false nil] nil] nil] nil])
    false)
 
-
-(= (__ '(:a nil ()))
+(= (bin-tree? '(:a nil ()))
    false)
