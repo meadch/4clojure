c4e0e9c4b0814cc5b6228e336129483c706b1a6b
meadch <meadch@gmail.com>
2020-10-24 06:43:52 -0700

    universal-computation-engine solution

--- src/universal-computation-engine.clj
+++ src/universal-computation-engine.clj
@@ -6,20 +6,29 @@
 ; Special Restrictions
 ; eval
 ; resolve
-; 
-(= 2 ((__ '(/ a b))
+
+(defn calc [exp]
+  (let [ops {'/ / '* * '+ + '- -}]
+    (fn compute [env]
+      (cond (number? exp) exp
+            (contains? env exp) (exp env)
+            (contains? ops exp) (exp ops)
+            :else (let [exp (map #((calc %) env) exp)] ;assume seq w/ operation in first position
+                    (apply (first exp) (rest exp)))))))
+
+(= 2 ((calc '(/ a b))
       '{b 8 a 16}))
 
-(= 8 ((__ '(+ a b 2))
+(= 8 ((calc '(+ a b 2))
       '{a 2 b 4}))
 
 (= [6 0 -4]
-   (map (__ '(* (+ 2 a)
+   (map (calc '(* (+ 2 a)
                 (- 10 b)))
         '[{a 1 b 8}
           {b 5 a -2}
           {a 2 b 11}]))
 
-(= 1 ((__ '(/ (+ x 2)
+(= 1 ((calc '(/ (+ x 2)
               (* 3 (+ y 1))))
       '{x 4 y 1}))
